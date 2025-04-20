acb48fc9f03cb6622f6ea6b095636778430488f2
Jesus Castro <jesusdavidcastror30@gmail.com>
2025-04-20 13:21:18 -0500

    Add class for test

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import PasswordChangeView
+from .views import PasswordChangeView,CreateUserTest
 
 urlpatterns = [
     path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain"),
     path("auth/refresh-token/", TokenRefreshView.as_view(), name="token_refresh"),
     path("auth/change-password/", PasswordChangeView.as_view(), name="change_password"),
+    path("test/", CreateUserTest.as_view(), name="logout"),
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,10 +1,12 @@
 # views.py
+from django.http import JsonResponse
 from rest_framework import status, permissions
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import PasswordChangeSerializer
-
+from .models import User
 
 class PasswordChangeView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -27,3 +29,28 @@ class PasswordChangeView(APIView):
                 status=status.HTTP_200_OK,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateUserTest(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        try:
+            user, created = User.objects.get_or_create(
+                username='jesusadmin1',
+                defaults={
+                    'password': 'jesusadmin123',
+                    'role': 'admin',
+                    'enabled': True
+                }
+            )
+
+            if not created:
+                return Response({'message': '⚠️ El usuario ya existe.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            user.set_password('jesusadmin123')
+            user.save()
+
+            return Response({'message': '✅ Usuario creado correctamente.'}, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
